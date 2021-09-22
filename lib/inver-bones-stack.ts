0d654fd751fabb97c4d2199eb2e0668bde57b1fd
Shaoqing Tan <bettercallshao@gmail.com>
2021-09-22 00:19:40 -0400

    Added amplify app

--- lib/inver-bones-stack.ts
+++ lib/inver-bones-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as amplify from "@aws-cdk/aws-amplify";
 
 export class InverBonesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const amplifyApp = new amplify.App(this, "inver", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "bettercallshao",
+        repository: "inver",
+        oauthToken: cdk.SecretValue.secretsManager("inver-bones", {
+          jsonField: "github",
+        }),
+      }),
+    });
+    const mainBranch = amplifyApp.addBranch("main");
   }
 }
